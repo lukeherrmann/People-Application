e294f0cc41aebd4d4b540938ae088e05acdd9c29
Luke Herrmann <herrmannluke@gmail.com>
2019-02-19 22:00:25 -0700

    Complete Basic Obj

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+  
+  validates :age, numericality: true
+  validates :name, :age, :gender, presence: true
+  
+  def basic_info
+    "#{self.name}, #{self.age}, #{self.gender}"
+   end
 end
